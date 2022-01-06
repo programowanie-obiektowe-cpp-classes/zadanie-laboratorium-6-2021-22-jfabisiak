c758634bef20a35f8e7e641b9471edceef61b55e
jfabisiak <jafabisak@gmail.com>
2022-01-06 03:30:59 +0100

    test na for_eacha

--- foo.hpp
+++ foo.hpp
@@ -11,8 +11,11 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret_v(people.size());
 
     std::for_each(people.begin(), people.end(), [&](Human& people){people.birthday();});
-   std::transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human& people){
-        return people.isMonster() ? 'n' : 'y';
+   std::for_each(people.begin(), people.end(), [&](Human& people){
+        if(people.isMonster()) 
+        ret_v.push_back('n');
+        else
+        ret_v.push_back('y');
         });
 
 
