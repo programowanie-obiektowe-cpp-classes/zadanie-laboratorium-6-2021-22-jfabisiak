83590404ef7ba95bf9dc18901cd98fcce155cc01
jfabisiak <jafabisak@gmail.com>
2022-01-06 03:29:03 +0100

    zmiana kierunku wpisywania

--- foo.hpp
+++ foo.hpp
@@ -11,7 +11,7 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret_v(people.size());
 
     std::for_each(people.begin(), people.end(), [&](Human& people){people.birthday();});
-   std::transform(people.rbegin(), people.rend(), ret_v.rbegin(), [](Human& people){
+   std::transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human& people){
         return people.isMonster() ? 'n' : 'y';
         });
 
