30fed640d2c52e31146f70acdddd37877c5f1bc3
jfabisiak <jafabisak@gmail.com>
2022-01-06 03:35:45 +0100

    powrot do działającej wersji

--- foo.hpp
+++ foo.hpp
@@ -11,11 +11,8 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret_v(people.size());
 
     std::for_each(people.begin(), people.end(), [&](Human& people){people.birthday();});
-   std::for_each(people.rbegin(), people.rend(), [&](Human& people){
-        if(people.isMonster()) 
-        ret_v.push_back('n');
-        else
-        ret_v.push_back('y');
+   std::transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human& people){
+        return people.isMonster() ? 'n' : 'y';
         });
 
 
